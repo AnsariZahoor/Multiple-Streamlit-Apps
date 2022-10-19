8eb153dbba1f96b22a31bd0f4c1f45d53de4b47a
zahoor <zahoor.ansari@ronianalytics.com>
2022-10-19 19:27:29 +0530

    nginx docker

---
+++ first_app/app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Streamlit APP :one:')
+st.text('This is a FIRST web app for testing NGINX reverse proxy')
---
+++ second_app/app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Streamlit APP :two:')
+st.text('This is a SECOND web app for testing NGINX reverse proxy')
